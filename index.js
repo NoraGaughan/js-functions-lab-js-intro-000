c845cd1fad4642f760abf9eda76c51155c1ad495
Nora Gaughan <noragaughan@gmail.com>
2020-07-09 21:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 function happyHolidays() {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays ${name}!`}
+function happyCustomHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
